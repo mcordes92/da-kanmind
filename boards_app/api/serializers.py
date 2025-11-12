a69e45058f83f0c9fe3fea5f534845af6c17de3a
Marvin Cordes <mcordes@cordes-hosting.net>
2025-11-12 12:24:09 +0100

    feat(api): add serializers for board details and updates

--- boards_app/api/serializers.py
+++ boards_app/api/serializers.py
@@ -22,8 +22,7 @@ class BoardListSerializer(serializers.ModelSerializer):
         ]
 
     def get_member_count(self, obj):
-        #return obj.members.count()
-        return 0
+        return obj.members.count()
 
     def get_ticket_count(self, obj):
         #return obj.tasks.count()
@@ -54,3 +53,66 @@ class BoardCreateSerializer(serializers.ModelSerializer):
         board.members.set(users)
 
         return board
+    
+class UserSerializer(serializers.ModelSerializer):
+    fullname = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'fullname']
+
+    def get_fullname(self, obj):
+        return obj.profile.full_name
+    
+class BoardDetailSerializer(serializers.ModelSerializer):
+    owner_id = serializers.ReadOnlyField(source='owner.id')
+    members = UserSerializer(many=True, read_only=True)
+    tasks = []  # Placeholder for tasks serialization
+
+    # TODO: Hier noch die Task mit aufnehmen!
+
+    class Meta:
+        model = Boards
+        fields = ['id', 'title', 'owner_id', 'members']
+
+class BoardUpdateSerializer(serializers.ModelSerializer):
+    members = serializers.ListField(
+        child=serializers.IntegerField(), write_only=True, required=False
+    )
+    title = serializers.CharField(required=False)
+
+    class Meta:
+        model = Boards
+        fields = ['title', 'members']
+
+    def validate_members(self, value):
+        valid_members = User.objects.filter(id__in=value)
+        missing_ids = set(value) - set(valid_members.values_list('id', flat=True))
+
+        if missing_ids:
+            raise serializers.ValidationError(
+                f"Ungültige Benutzer-IDs: {', '.join(map(str, missing_ids))}"
+            )
+        return value
+    
+    def update(self, instance, validated_data):
+        members = validated_data.pop("members", None)
+        title = validated_data.get("title", None)
+
+        if title is not None:
+            instance.title = title
+
+        if members is not None:
+            users = User.objects.filter(id__in=members)
+            instance.members.set(users)
+
+        instance.save()
+        return instance
+    
+class BoardUpdateResSerializer(serializers.ModelSerializer):
+    owner_data = UserSerializer(source='owner', read_only=True)
+    members_data = UserSerializer(source='members', many=True, read_only=True)
+
+    class Meta:
+        model = Boards
+        fields = ['id', 'title', 'owner_data', 'members_data']
--- boards_app/api/views.py
+++ boards_app/api/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-from .serializers import BoardListSerializer, BoardCreateSerializer
+from .serializers import BoardListSerializer, BoardCreateSerializer, BoardDetailSerializer, BoardUpdateSerializer, BoardUpdateResSerializer
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import User
 from boards_app.models import Boards
@@ -18,7 +18,9 @@ class BoardsViewSet(viewsets.ModelViewSet):
             return BoardListSerializer
         elif self.action == "create":
             return BoardCreateSerializer
-        return BoardListSerializer
+        elif self.action in ["update", "partial_update"]:
+            return BoardUpdateSerializer
+        return BoardDetailSerializer
 
 
     def get_queryset(self):
@@ -30,3 +32,26 @@ class BoardsViewSet(viewsets.ModelViewSet):
         serializer.is_valid(raise_exception=True)
         board = serializer.save() 
         return Response(BoardListSerializer(board).data, status=status.HTTP_201_CREATED)
+    
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.get('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial, context={"request": request})
+        serializer.is_valid(raise_exception=True)
+        board = serializer.save()
+        return Response(BoardUpdateResSerializer(board).data, status=status.HTTP_200_OK)
+    
+    def partial_update(self, request, *args, **kwargs):
+        kwargs['partial'] = True
+        return self.update(request, *args, **kwargs)
+    
+    def destroy(self, request, *args, **kwargs):
+        board = self.get_object()
+
+        if board.owner != request.user:
+            return Response({"detail": "Nur der Eigentümer darf dieses Board löschen."}, status=status.HTTP_403_FORBIDDEN)
+
+        # TODO: Task delete einbauen
+
+        board.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
